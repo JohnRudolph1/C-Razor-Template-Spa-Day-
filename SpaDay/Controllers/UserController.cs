7fd0851632f43deefd14a5581625804cb2261576
JohnRudolph1 <johnrudolph963@gmail.com>
2021-12-16 11:11:16 -0600

    finished chapter 14 studio

---
+++ SpaDay/Controllers/UserController.cs
@@ -0,0 +1,39 @@
+﻿using SpaDay.Models;
+
+using Microsoft.AspNetCore.Mvc;
+
+namespace SpaDay.Controllers
+{
+    public class UserController : Controller
+
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        
+        public IActionResult Add()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [Route("/user/add")]
+        public IActionResult SubmitAddUserForm(User newUser, string verify)
+        {
+            if (verify == newUser.Password)
+            {
+                ViewBag.user = newUser.Username;
+                return View("Index");
+            }
+
+            else
+            {
+                ViewBag.error = "Both Passwords Must Match";
+                return View("Add");
+            }
+        }
+    }
+}
+
